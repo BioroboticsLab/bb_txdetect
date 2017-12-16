7e66398d390250119158d0f45b4550f76aca68ad
mathis <mathis.hocke@fu-berlin.de>
2017-12-16 17:24:16 +0100

    refactoring

---
+++ init_steps.py
@@ -0,0 +1,20 @@
+import map_data
+from tqdm import tqdm 
+
+map_data.setSnsStyle("ticks")
+db = map_data.connect()
+gt_events, gt_data = map_data.load_gt_data()
+frame_fc_map = map_data.get_frame_container_info_for_frames(db, list(map_data.get_all_frame_ids(gt_events)))
+frame_to_fc_map = map_data.get_frame_to_fc_path_dict(frame_fc_map)
+map_data.map_additional_data_to_events(gt_events, frame_to_fc_map)
+#event_df = pandas.concat([event.df for event in gt_events], axis=0, ignore_index=True)
+
+for event in tqdm(gt_events):
+    event.df["trophallaxis_observed"] = event.trophallaxis_observed
+        
+verified_events = list(filter(lambda x: x.trophallaxis_observed == True, gt_events))
+
+verified = gt_data[gt_data.trophallaxis_observed == 'y']
+durations = verified['trophallaxis_end_frame_nr'] - verified['trophallaxis_start_frame_nr'] + 1
+
+print("done")
