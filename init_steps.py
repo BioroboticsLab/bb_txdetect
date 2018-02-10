bbfa83c651ac497de030d43a27e970982e095c38
mathis <mathis.hocke@fu-berlin.de>
2018-02-10 21:42:00 +0100

    interpolate all missing frames in event

--- init_steps.py
+++ init_steps.py
@@ -1,5 +1,4 @@
 from map_data import DataMapper, setSnsStyle
-from tqdm import tqdm
 
 setSnsStyle("ticks")
 dm = DataMapper()
@@ -9,15 +8,4 @@ frame_fc_map = dm.get_frame_container_info_for_frames(list(dm.get_all_frame_ids(
 frame_to_fc_map = dm.get_frame_to_fc_path_dict(frame_fc_map)
 dm.map_additional_data_to_events(frame_to_fc_map)
 dm.map_bee_ids()
-dm.map_frames_before_after(num_frames=8)
-
-#
-#for event in tqdm(gt_events):
-#    event.df["trophallaxis_observed"] = event.trophallaxis_observed
-#
-#verified_events = list(filter(lambda x: x.trophallaxis_observed == True, gt_events))
-#
-#verified = gt_data[gt_data.trophallaxis_observed == 'y']
-#durations = verified['trophallaxis_end_frame_nr'] - verified['trophallaxis_start_frame_nr'] + 1
-#
-#print("done")
+dm.map_observations(padding=8)
--- map_data.py
+++ map_data.py
@@ -1,16 +1,14 @@
+import math
+import json
+from typing import List, Dict
 import numpy as np
 import pandas as pd
 import matplotlib
-import json
 from tqdm import tqdm
 import seaborn as sns
 import psycopg2
-import bb_binary
 import numba
-import math
-
-from psycopg2.extensions import connection
-from typing import List, Dict
+import bb_binary
 
 
 class Event(object):
@@ -20,12 +18,7 @@ class Event(object):
     begin_frame_idx, end_frame_idx = -1, -1
     trophallaxis_observed = False
     df = None
-    frames_ids_before = None
-    frames_before = None
-    frames_ids_after = None
-    frames_after = None
-    not_nans_before = None
-    not_nans_after = None
+    observations = []
 
     def __init__(self, row):
         self.detection_ids = (
@@ -47,11 +40,26 @@ class Event(object):
             yield int(detection_id[1:].split("d")[0])
 
 
+class Observation(object):
+    def __init__(self, frame_id, xs, ys, orientations):
+        self.frame_id = frame_id
+        self.xs = xs
+        self.ys = ys
+        self.orientations = orientations
+        self.trophallaxis_observed = None
+
+    def file_name(self) -> str:
+        return "{}_{}_{}_{}_{}_{}_{}_{}".format(self.frame_id,
+                                                *self.xs,
+                                                *self.ys,
+                                                *self.orientations,
+                                                self.trophallaxis_observed)
+
+
 def np_float_to_int(x: np.float) -> int:
     if(np.isnan(x)):
         return -1
-    else:
-        return int(x)
+    return int(x)
 
 
 def setSnsStyle(style: str):
@@ -72,8 +80,8 @@ class DataMapper:
     db = None
 
     def connect(self):
-        self.db = psycopg2.connect("dbname='beesbook' user='reader' host='localhost' password='reader'")
-
+        self.db = psycopg2.connect(
+            "dbname='beesbook' user='reader' host='localhost' password='reader'")
 
     def load_gt_data(self, path: str = 'csv/ground_truth_concat.csv'):
         gt_data = pd.read_csv(path, index_col=0)
@@ -82,8 +90,8 @@ class DataMapper:
         self.gt_events = []
         for i in tqdm(range(gt_data.shape[0])):
             self.gt_events.append(Event(gt_data.iloc[i, :]))
-        tqdm.write("Ground truth events loaded: {}".format(len(self.gt_events)))
-
+        tqdm.write("Ground truth events loaded: {}".format(
+            len(self.gt_events)))
 
     # Map frame container info to event frames.
     # The frame container will be used to load the positional data.
@@ -112,13 +120,11 @@ class DataMapper:
                                     columns=("frame_id", "fc_id", "fc_path", "video_name"))
         return frame_fc_map
 
-
     # TODO replace bb_binary
     def load_frame_container(self, fname):
         """Loads :obj:`.FrameContainer` from this filename."""
         with open(fname, 'rb') as f:
             return bb_binary.FrameContainer.read(f, traversal_limit_in_words=2**63)
-
 
     def get_all_frame_ids(self):
         all_frame_ids = set()
@@ -127,7 +133,6 @@ class DataMapper:
                 all_frame_ids.add(frame_id)
         tqdm.write("Unique frame ids: {}".format(len(all_frame_ids)))
         return all_frame_ids
-
 
     def get_frame_to_fc_path_dict(self, frame_fc_map: pd.DataFrame) -> Dict:
         fc_files = {}
@@ -140,13 +145,11 @@ class DataMapper:
                 frame_to_fc_map[frame] = fc_files[fc_path]
         return frame_to_fc_map
 
-
     def split_detection_id(self, detection_id: str) -> (int, int):
         frame_id, detection_idx = detection_id[1:].split("d")
         frame_id = int(frame_id)
         detection_idx = int(detection_idx.split("c")[0])
         return frame_id, detection_idx
-
 
     def map_additional_data_to_events(self, frame_to_fc_map):
         """For every event, map additional data.
@@ -157,7 +160,8 @@ class DataMapper:
             ts_set = set()
             for bee in range(len(event.detection_ids)):
                 for detection_id in event.detection_ids[bee]:
-                    frame_id, detection_idx = self.split_detection_id(detection_id)
+                    frame_id, detection_idx = self.split_detection_id(
+                        detection_id)
                     fc = frame_to_fc_map[frame_id]
                     frame = None
 
@@ -200,44 +204,43 @@ class DataMapper:
             event.df.columns = list(abee.columns) + list(bbee.columns)
             assert len(ts_set) == 0
 
-
     def map_bee_ids(self):
         tqdm.write("map bee ids")
         for event in tqdm(self.gt_events):
             event.bee_ids = (self.get_bee_id(*self.split_detection_id(event.detection_ids[0][0])),
                              self.get_bee_id(*self.split_detection_id(event.detection_ids[1][0])))
 
-
-
     def get_bee_id(self, frame_id: int, detection_idx: int):
         cur = self.db.cursor()
         if not self.get_bee_id_is_prepared:
-            cur.execute("PREPARE get_bee_id AS SELECT bee_id FROM bb_detections WHERE frame_id = $1 AND detection_idx = $2;")
+            cur.execute(
+                "PREPARE get_bee_id AS SELECT bee_id FROM bb_detections WHERE frame_id = $1 AND detection_idx = $2;")
             self.get_bee_id_is_prepared = True
         cur.execute("EXECUTE get_bee_id (%s, %s);", (frame_id, detection_idx))
         result = cur.fetchone()
         if result:
             return result[0]
-        else:
-            return -1
+        return -1
 
-
-    def get_timestamp(self, frame_id: int):
-        cur = self.db.cursor()
-        cur.execute("SELECT timestamp FROM plotter_frame where frame_id = %s", (frame_id,))
-        return cur.fetchone()[0]
-
-
-    def map_frames_before_after(self, num_frames: int):
+    def map_observations(self, padding: int):
         tqdm.write("map frames before and after")
         for event in tqdm(self.gt_events):
             frame_ids = list(event.frame_ids)
-            self.set_frames_before_after(frame_id_begin=frame_ids[0], 
-                                         frame_id_end=frame_ids[len(list(event.frame_ids))-1],
-                                         bee_ids=event.bee_ids, 
-                                         event=event,
-                                         num_frames=num_frames)
-
+            event.observations = self.get_all_frames(frame_id_begin=frame_ids[0],
+                                                     frame_id_end=frame_ids[len(
+                                                         list(event.frame_ids))-1],
+                                                     bee_ids=event.bee_ids,
+                                                     frame_padding_length=padding)
+            event.padding = padding
+            i = 0
+            for obs in event.observations:
+                if i < padding or i >= len(event.observations) - padding:
+                    obs.trophallaxis_observed = None
+                elif i >= event.begin_frame_idx and i <= event.end_frame_idx:
+                    obs.trophallaxis_observed = True
+                else:
+                    obs.trophallaxis_observed = False
+                i += 1
 
     def get_position_and_orientation(self, frame_id: int, bee_ids: (int, int)):
         cursor = self.db.cursor()
@@ -245,86 +248,89 @@ class DataMapper:
                        (frame_id, *bee_ids))
         return list(cursor)
 
+    def get_timestamp(self, frame_id: int):
+        cur = self.db.cursor()
+        cur.execute(
+            "SELECT timestamp FROM plotter_frame where frame_id = %s LIMIT 1", (frame_id,))
+        return cur.fetchone()[0]
 
-    def get_neighbour_frames(self, frame_id, n_frames=None, seconds=None, mode: str = 'around'):
-        seconds = seconds or (n_frames / 3 if n_frames else 5.0)
-        
+    def get_neighbour_frames(self, frame_id1, frame_id2, n_frames=None, seconds=None, mode: str = 'around'):
+
         with psycopg2.connect("dbname='beesbook' user='reader' host='localhost' password='reader'",
                               application_name="get_neighbour_frames") as db:
-            cursor = db.cursor()
-            cursor.execute("SELECT index, fc_id, timestamp FROM plotter_frame WHERE frame_id = %s LIMIT 1", (frame_id,))
-            f_index, fc_id, timestamp = cursor.fetchone()
-            
-            #print(f"Frame index: {f_index}, timestamp: {timestamp}, container ID: {fc_id}")
+
+            seconds = seconds or (n_frames / 3 if n_frames else 5.0)
+            timestamp1 = self.get_timestamp(frame_id1)
+            timestamp2 = self.get_timestamp(frame_id2)
 
             if mode == 'before':
-                ts1 = timestamp - seconds
-                ts2 = timestamp
+                ts1 = timestamp1 - seconds
+                ts2 = timestamp2
             if mode == 'after':
-                ts1 = timestamp 
-                ts2 = timestamp + seconds
+                ts1 = timestamp1
+                ts2 = timestamp2 + seconds
             if mode == 'around':
-                ts1 = timestamp - seconds
-                ts2 = timestamp + seconds
-            
-            cursor.execute("SELECT timestamp, frame_id, fc_id FROM plotter_frame WHERE timestamp >= %s AND timestamp <= %s", (ts1, ts2))
+                ts1 = timestamp1 - seconds
+                ts2 = timestamp2 + seconds
+
+            cursor = db.cursor()
+            cursor.execute(
+                "SELECT fc_id FROM plotter_frame WHERE frame_id = %s LIMIT 1", (frame_id1,))
+            fc_id = cursor.fetchone()[0]
+
+            cursor.execute(
+                "SELECT timestamp, frame_id, fc_id FROM plotter_frame WHERE timestamp >= %s AND timestamp <= %s", (ts1, ts2))
             results = list(cursor)
             containers = {fc_id for (_, _, fc_id) in results}
-            
+
             cursor.execute("PREPARE fetch_container AS "
-                       "SELECT CAST(SUBSTR(video_name, 5, 1) AS INT) FROM plotter_framecontainer "
-                       "WHERE id = $1")
+                           "SELECT CAST(SUBSTR(video_name, 5, 1) AS INT) FROM plotter_framecontainer "
+                           "WHERE id = $1")
             cursor.execute("EXECUTE fetch_container (%s)", (fc_id,))
             target_cam = cursor.fetchone()[0]
-            #print (f"Target cam: {target_cam}")
             matching_cam = set()
             for container in containers:
                 cursor.execute("EXECUTE fetch_container (%s)", (container,))
                 cam = cursor.fetchone()[0]
-                #print (f"\tChecking cam: {cam}...")
                 if cam == target_cam:
                     matching_cam.add(container)
-            results = [(timestamp, frame_id, target_cam) for (timestamp, frame_id, fc_id) in results if fc_id in matching_cam]
+            results = [(timestamp, frame_id, target_cam) for (
+                timestamp, frame_id, fc_id) in results if fc_id in matching_cam]
             return sorted(results)
 
-
-    def set_frames_before_after(self, frame_id_begin: int, frame_id_end: int, bee_ids: (int, int), event: Event, num_frames: int = None, seconds: int = None):
-        timestamp_begin = self.get_timestamp(frame_id=frame_id_begin)
-        timestamp_end = self.get_timestamp(frame_id=frame_id_end)
-        seconds = num_frames / 3
-
-        frames = self.get_neighbour_frames(frame_id=frame_id_begin, n_frames=num_frames, mode='before')
-        event.frame_ids_before = [frame_id for (timestamp, frame_id, fc_id) in frames]
-        event.frames_before, event.not_nans_a_before, event.not_nans_b_before = self.interpolate(event.frame_ids_before, bee_ids)
-
-        frames = self.get_neighbour_frames(frame_id=frame_id_end, n_frames=num_frames, mode='after')
-        event.frame_ids_after = [frame_id for (timestamp, frame_id, fc_id) in frames]
-        event.frames_after, event.not_nans_a_after, event.not_nans_b_after = self.interpolate(event.frame_ids_after, bee_ids)
-
+    def get_all_frames(self, frame_id_begin: int, frame_id_end: int, bee_ids: (int, int), frame_padding_length: int = None):
+        frames = self.get_neighbour_frames(
+            frame_id1=frame_id_begin, frame_id2=frame_id_end, n_frames=frame_padding_length, mode='before')
+        frame_ids = [frame_id for (timestamp, frame_id, fc_id) in frames]
+        interpolated = self.interpolate(frame_ids, bee_ids)
+        return [Observation(frame_id=frame_ids[i],
+                            xs=(interpolated[i][0], interpolated[i][3]),
+                            ys=(interpolated[i][1], interpolated[i][4]),
+                            orientations=(interpolated[i][2], interpolated[i][5])) for i in range(len(frame_ids))]
 
     def interpolate(self, frame_ids, bee_ids):
         results = np.empty((len(frame_ids), 6), dtype=np.float32)
-        results[:,:] = np.nan
+        results[:, :] = np.nan
         i = 0
         for frame_id in frame_ids:
-            detections = self.get_position_and_orientation(frame_id=frame_id, bee_ids=bee_ids)
+            detections = self.get_position_and_orientation(
+                frame_id=frame_id, bee_ids=bee_ids)
             for d in detections:
                 if d[0] == bee_ids[0]:
-                    results[i,:3] = [d[1], d[2], d[3]]
+                    results[i, :3] = [d[1], d[2], d[3]]
                 else:
-                    results[i,3:] = [d[1], d[2], d[3]]
+                    results[i, 3:] = [d[1], d[2], d[3]]
             i += 1
 
         # now results may have gaps with nans, but all frames are included
 
-        not_nans_a = interpolate_trajectory(results[:,:3])
-        not_nans_b = interpolate_trajectory(results[:,3:])
+        interpolate_trajectory(results[:, :3])
+        interpolate_trajectory(results[:, 3:])
+        return results
 
-        return (results, not_nans_a, not_nans_b)
-        
 
 @numba.njit
-def short_angle_dist(a0,a1):
+def short_angle_dist(a0, a1):
     max = math.pi*2
     da = (a1 - a0) % max
     return 2*da % max - da
@@ -332,36 +338,36 @@ def short_angle_dist(a0,a1):
 
 @numba.njit(numba.float32[:](numba.float32[:, :]))
 def interpolate_trajectory(trajectory):
-   
-    nans = np.isnan(trajectory[:,0])
+
+    nans = np.isnan(trajectory[:, 0])
     not_nans = ~nans
-   
+
     nans_idx = np.where(nans)[0]
     valid_idx = np.where(not_nans)[0]
     if len(valid_idx) < 2:
         return np.zeros(shape=(trajectory.shape[0]), dtype=np.float32)
-   
+
     # Interpolate gaps.
     for i in nans_idx:
         # Find closest two points to use for interpolation.
         begin_t = np.searchsorted(valid_idx, i) - 1
         if begin_t == len(valid_idx) - 1:
-            begin_t -= 1 # extrapolate right
+            begin_t -= 1  # extrapolate right
         elif begin_t == -1:
-            begin_t = 0 # extrapolate left
-       
+            begin_t = 0  # extrapolate left
+
         begin_t_idx = valid_idx[begin_t]
         end_t_idx = valid_idx[begin_t + 1]
-       
+
         last_t = trajectory[begin_t_idx]
         next_t = trajectory[end_t_idx]
         dx = (end_t_idx - begin_t_idx) / 3.0
         m = [(next_t[0] - last_t[0]) / dx,
              (next_t[1] - last_t[1]) / dx,
              short_angle_dist(last_t[2], next_t[2]) / dx]
- 
+
         dt = (i - begin_t_idx) / 3.0
         e = [m[i] * dt + last_t[i] for i in range(3)]
         trajectory[i] = e
-       
+
     return not_nans.astype(np.float32)
