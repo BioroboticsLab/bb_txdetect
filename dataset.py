982b746e4728ace07896a6b9aaaf4ecf047ab7ab
mathis <mathis.hocke@fu-berlin.de>
2018-03-26 14:58:45 +0200

    scale down images; added shuffling; fixed memory leak

--- dataset.py
+++ dataset.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 from glob import glob
 from typing import List
 import numpy as np
@@ -7,16 +7,19 @@ import itertools
 import torchvision.transforms as transforms
 from torch.utils.data.dataset import Dataset, Subset
 from skimage.io import imread
+from skimage.transform import resize
 
 
 class TrophallaxisDataset(Dataset):
-    def __init__(self, item_depth: int, transform=None):
+    def __init__(self, item_depth: int, transform=None, image_size=(128,128), hide_tags=True):
+        self.tags = "_hidden_tags" if hide_tags else ""
         self.transformations = transform if transform else transforms.Compose([transforms.ToTensor()])
-        self.all_paths = sorted(glob("images/*/0/*.png"))
+        self.all_paths = sorted(glob("images/*/0{}/*.png".format(self.tags)))
         self.item_depth = item_depth
         self.y_indices = self._indices_by_label("y")
         self.n_indices = self._indices_by_label("n")
         self.count = len(self.y_indices) + len(self.n_indices)
+        self.image_size = image_size
 
         self.grouped_by_event = {}
         self.event_labels = []
@@ -47,12 +50,16 @@ class TrophallaxisDataset(Dataset):
         paths = [*before, path, *after]
 
         # randomly choose rotation
-        if random() > 0.5:
+        if random.random() > 0.5:
             for i, p in enumerate(paths):
-                split = p.split("/0/")
-                paths[i] = split[0] + "/1/" + split[1]
+                split = p.split("/0{}/".format(self.tags))
+                paths[i] = split[0] + "/1{}/".format(self.tags) + split[1]
 
         images = [imread(path) for path in paths]
+        assert self.image_size[0] == self.image_size[1]
+        assert self.image_size[0] <= 128
+        if self.image_size[0] < 128:
+            images = [resize(img, self.image_size) for img in images]
         data = np.dstack(images) if len(images) > 1 else images[0]
         data = self.transformations(data)
         return (data, label)
@@ -68,8 +75,8 @@ class TrophallaxisDataset(Dataset):
         return int(path.split("/")[1].split("_")[0])
 
     def _get_subset(self, split_ratio, train: bool) -> Subset:
-        y_events = [i for i,label in enumerate(self.event_labels) if label]
-        n_events = [i for i,label in enumerate(self.event_labels) if not label]
+        y_events = shuffle([i for i,label in enumerate(self.event_labels) if label])
+        n_events = shuffle([i for i,label in enumerate(self.event_labels) if not label])
 
         if train:
             y_events = y_events[:int(len(y_events)*split_ratio)]
@@ -80,8 +87,9 @@ class TrophallaxisDataset(Dataset):
 
         print("ratio", len(y_events) / (len(y_events)+len(n_events)), "train" if train else "test")
 
-        indices = [self.grouped_by_event[i] for i in y_events]
-        indices += [self.grouped_by_event[i] for i in n_events]
+        events = shuffle([*y_events, *n_events])
+
+        indices = [self.grouped_by_event[i] for i in events]
 
         return Subset(dataset=self, indices=list(itertools.chain.from_iterable(indices)))
 
@@ -96,3 +104,7 @@ class TrophallaxisDataset(Dataset):
         test_folders = set([self._folder_index(self.all_paths[i]) for i in test.indices])
         return train_folders & test_folders
 
+
+def shuffle(l: list, seed=42) -> list:
+    random.seed(seed)
+    return random.sample(l, len(l))
--- train.py
+++ train.py
@@ -1,101 +1,114 @@
+from functools import reduce, partial
+from pathlib import Path
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
+from sklearn.metrics import f1_score, confusion_matrix
 
 import dataset
 import resnet
 
-def test(model, optimizer, criterion, testloader):
-    trainloss = 0
-    trainloss_y = 0
-    trainloss_n = 0
-    for i, data in enumerate(testloader, 0):
-        # get the inputs
+MODELPATH = "saved_model"
+
+def run_epoch(model, optimizer, criterion, loader, train: bool):
+    if train:
+        model.train()
+    else:
+        model.eval()
+
+    loss = 0.0
+
+    y_true = []
+    y_pred = []
+    batchlens = []
+    for i, data in enumerate(loader, 0):
         inputs, labels = data
-        label = labels[0]
-
-        # wrap them in Variable
-        inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-
-        # zero the parameter gradients
+        inputs, labels = Variable(inputs.cuda(), volatile=not train), Variable(labels.cuda(), volatile= not train)
+        
         optimizer.zero_grad()
-
-        # forward + backward + optimize
         outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        if label == 1:
-            trainloss_y += loss.data[0]
-        else:
-            trainloss_n += loss.data[0]
-        trainloss += loss.data[0]
-    return trainloss, trainloss_y, trainloss_n
 
-def train(model, optimizer, criterion, trainloader, testloader):
-    losses = []
-    test_losses = []
-    for epoch in range(100):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs
-            inputs, labels = data
-
-            # wrap them in Variable
-            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-            
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
+        if train:
+            batchloss = criterion(outputs, labels)
+            batchloss.backward()
             optimizer.step()
+            loss += batchloss.data[0]
 
-            # print statistics
-            running_loss += loss.data[0]
-            if i % 20 == 19:
-                losses.append(running_loss)
-                with open('train_losses.csv','a') as f:
-                    f.write("{}\n".format(running_loss))
-                running_loss = 0.0
-                
-        print("epoch", epoch, "finished")
-        testloss = test(model,optimizer,criterion,testloader)
-        with open('test_losses.csv','a') as f:
-            f.write("{},{},{}\n".format(*testloss))
-        test_losses.append(testloss)
+        y_true += [y for y in labels.data]
+        y_pred += [0 if y[0] > y[1] else 1 for y in outputs.data]
+        batchlens.append(len(y_true))
+
+    print("batch lengths avg: {}, num batches: {}".format(sum(batchlens)/i,i))
+    score = f1_score(y_true=y_true, y_pred=y_pred)
+    conmat = confusion_matrix(y_true=y_true, y_pred=y_pred)
+            
+    loss /= i
+    score /= i
+    return conmat, score, loss 
+
+
+def csv(vals: list) -> str:
+    return reduce(lambda x,y: str(x) + "," + str(y), vals)
+
+
+def format_stats(conmat: np.ndarray, *args) -> str:
+    return csv([*args, "[" + csv([int(x) for x in [*conmat[0], *conmat[1]]]) + "]"])
+
+
+def run_training(model, optimizer, criterion, trainloader, testloader, start_epoch):
+    run = partial(run_epoch, model=model, 
+                  optimizer=optimizer, criterion=criterion)
+    for epoch in range(start_epoch, 1000):
+        print("train epoch", epoch)
+        out = "[" + format_stats(*run(loader=trainloader, train=True))
+        print("test epoch", epoch)
+        conmat, _, score = run(loader=testloader, train=False)
+        out += "," + format_stats(conmat, score) + "]\n"
+
+        with open("train_stats.csv", "a") as f:
+            f.write(out)
+
         state = {
-            'epoch': epoch,
-            'state_dict': model.state_dict(),
-            'optimizer': optimizer.state_dict(),
-            'test_losses': test_losses
+            "epoch": epoch,
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict()
         }
-        torch.save(state, "saved_model")
+        torch.save(state, MODELPATH)
+        torch.cuda.empty_cache()
 
-    print('Finished Training')
+
+def restore(model, optimizer):
+    if Path(MODELPATH).exists():
+        state = torch.load(MODELPATH)
+        model.load_state_dict(state["state_dict"])
+        optimizer.load_state_dict(state["optimizer"])
+        return state["epoch"] + 1
+    else:
+        return 0
+
 
 def main():
-    ds = dataset.TrophallaxisDataset(item_depth=3)
+    ds = dataset.TrophallaxisDataset(item_depth=3, image_size=(64,64))
     trainset = ds.trainset()
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=16,
-                                                shuffle=True, num_workers=2)
-
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+                                              shuffle=True, num_workers=2)
     testset = ds.testset()
-    testloader = torch.utils.data.DataLoader(testset, batch_size=1,
-                                              shuffle=False, num_workers=2)
-
-
-
-    model = resnet.resnet34()
-    model.cuda()
-
+    testloader = torch.utils.data.DataLoader(testset, batch_size=64,
+                                             shuffle=False, num_workers=2)
+    model = resnet.resnet18()
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    train(model,optimizer,criterion,trainloader,testloader)
+    epoch = restore(model,optimizer)
+
+    model.cuda()
+    print("starting training from epoch", epoch)
+    run_training(model,optimizer,criterion,trainloader,testloader,epoch)
+
 
 if __name__ == "__main__":
     main()
